0db40705ae9f803b5ebce161090dbf6172868248
contaJoseph <contacodejoseph@gmail.com>
2024-03-15 01:17:38 -0300

    fibonacci sequence

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php 
 
-echo "Hello World";
+function fibonacci($n)
+{
+    $arr = [0, 1];
+
+    for ($i = 1; $i < $n; $i++)
+    {
+        array_push($arr, $arr[$i] + $arr[$i-1]);
+    }
+
+    return $arr;
+}
+
+print_r(fibonacci(10));
